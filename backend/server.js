38f00292d24d43f5e258bfd87a2cb09dee005835
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-06 12:57:51 +0530

    basic server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.get("/api/goals", (req, res) => {
+  res.status(200).json({ message: "Get Goals" });
+});
+
+app.listen(port, () =>
+  console.log(`Server is running in port http://localhost:${port}`)
+);
