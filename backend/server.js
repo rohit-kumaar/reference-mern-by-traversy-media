963011805dd406d0a1dd9973de573bf3ceec9306
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-06 13:03:57 +0530

    initial routers

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,7 @@ const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-app.get("/api/goals", (req, res) => {
-  res.status(200).json({ message: "Get Goals" });
-});
+app.use("/api/goals", require("./routes/goalRouters"));
 
 app.listen(port, () =>
   console.log(`Server is running in port http://localhost:${port}`)
