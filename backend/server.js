649ddd19186e72a1e1c1a5f6dff82be3796b8e97
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-07 17:11:50 +0530

    error and exception handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/goals", require("./routes/goalRouters"));
 
 app.listen(port, () =>
